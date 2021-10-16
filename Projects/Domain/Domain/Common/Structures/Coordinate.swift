9cdcd6dabd8ee6ce0a66b5ebcd9b2ddd592ed038
Ekrem <eko_1994@hotmail.com>
2021-10-16 19:28:13 +0300

    feature Coordinate has distance calculation

--- Projects/Domain/Domain/Common/Structures/Coordinate.swift
+++ Projects/Domain/Domain/Common/Structures/Coordinate.swift
@@ -4,10 +4,18 @@
 //
 //  Created by Ekrem Duvarbasi on 16.10.2021.
 //
+import Foundation
 
 public struct Coordinate: Equatable {
     public static var zero: Coordinate { .init(x: 0, y: 0) }
     
-    public var x: Int
-    public var y: Int
+    public var x: Double
+    public var y: Double
+    
+    public func distance(from target: Coordinate) -> Double {
+        let x = pow(Double(target.x - x), 2)
+        let y = pow(Double(target.y - y), 2)
+        
+        return sqrt(x + y)
+    }
 }
