8b5c5c10e28dd262314c8f0b18f4638ce7648825
Ekrem <eko_1994@hotmail.com>
2021-10-16 17:29:17 +0300

    add Spacestation

---
+++ Projects/Domain/Domain/Model/SpaceStation.swift
@@ -0,0 +1,14 @@
+//
+//  SpaceStation.swift
+//  Domain
+//
+//  Created by Ekrem Duvarbasi on 16.10.2021.
+//
+
+public struct SpaceStation {
+    public var name: String
+    public var coordinate: Coordinate
+    public var capacity: Int64
+    public var stock: Int64
+    public var need: Int64
+}
