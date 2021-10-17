d32202477683ec38309014f818d8dded04c39b75
Ekrem <eko_1994@hotmail.com>
2021-10-17 15:25:51 +0300

    add iOS Project

---
+++ Projects/93652c41ebc5e7170a98811219ab09e0/93652c41ebc5e7170a98811219ab09e0/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  93652c41ebc5e7170a98811219ab09e0
+//
+//  Created by Ekrem Duvarbasi on 17.10.2021.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Projects/93652c41ebc5e7170a98811219ab09e0/93652c41ebc5e7170a98811219ab09e0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  93652c41ebc5e7170a98811219ab09e0
+//
+//  Created by Ekrem Duvarbasi on 17.10.2021.
+//
+
+import UIKit
+import Presentation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
