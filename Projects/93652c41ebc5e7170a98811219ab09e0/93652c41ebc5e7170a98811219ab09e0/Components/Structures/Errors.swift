21d8293af3f6d19bdccea8599ae4709d1b318da8
Ekrem <eko_1994@hotmail.com>
2021-10-17 16:04:55 +0300

    add ApiError

---
+++ Projects/93652c41ebc5e7170a98811219ab09e0/93652c41ebc5e7170a98811219ab09e0/Components/Structures/Errors.swift
@@ -0,0 +1,12 @@
+//
+//  Errors.swift
+//  93652c41ebc5e7170a98811219ab09e0
+//
+//  Created by Ekrem Duvarbasi on 17.10.2021.
+//
+
+import Domain
+
+struct ApiError: AppError {
+    var message: String
+}
