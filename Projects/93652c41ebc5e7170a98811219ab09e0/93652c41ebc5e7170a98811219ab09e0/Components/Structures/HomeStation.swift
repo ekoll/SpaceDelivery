7a05ee58fecd798689866b5dc2ef952e3fde5102
Ekrem <eko_1994@hotmail.com>
2021-10-17 17:15:02 +0300

    add HomeStation

---
+++ Projects/93652c41ebc5e7170a98811219ab09e0/93652c41ebc5e7170a98811219ab09e0/Components/Structures/HomeStation.swift
@@ -0,0 +1,13 @@
+//
+//  HomeStation.swift
+//  93652c41ebc5e7170a98811219ab09e0
+//
+//  Created by Ekrem Duvarbasi on 17.10.2021.
+//
+
+import Domain
+
+public struct HomeStation {
+    var name: String
+    var coordinate: Coordinate
+}
