e3ed79c5c8e66bb41b8f3a3b0290d5d584bd01e2
Ekrem <eko_1994@hotmail.com>
2021-10-17 14:56:27 +0300

    feature SpaceStation has public initializer

--- Projects/Domain/Domain/Model/SpaceStation.swift
+++ Projects/Domain/Domain/Model/SpaceStation.swift
@@ -6,12 +6,21 @@
 //
 
 public struct SpaceStation {
-    private(set) public var name: String = ""
-    private(set) public var coordinate: Coordinate = .zero
-    private(set) public var capacity: Int64 = 0
-    private(set) public var stock: Int64 = 0
-    private(set) public var need: Int64 = 0
-    internal(set) public var isFavorite: Bool = false
+    private(set) public var name: String
+    private(set) public var coordinate: Coordinate
+    private(set) public var capacity: Int64
+    private(set) public var stock: Int64
+    private(set) public var need: Int64
+    internal(set) public var isFavorite: Bool
+    
+    public init(name: String = "", coordinate: Coordinate = .zero, capacity: Int64 = 0, stock: Int64 = 0, need: Int64 = 0, isFavorite: Bool = false) {
+        self.name = name
+        self.coordinate = coordinate
+        self.capacity = capacity
+        self.stock = stock
+        self.need = need
+        self.isFavorite = isFavorite
+    }
     
     internal mutating func add(stock: Int64) {
         let stockToAdd = min(stock, need, capacity - self.stock)
